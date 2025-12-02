889f97ee084b5ecbec071fa8ab9ac962f7dbf6f0
Parth Vasdewani <imparthvasdewani@gmail.com>
2025-12-02 11:09:20 +0530

    Added tick consumer and producer apps to settings

--- tick_system/settings.py
+++ tick_system/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Adding apps
+    'tick_cosumer',
+    'tick_producer',
 ]
 
 MIDDLEWARE = [
