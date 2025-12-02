94824e4d6178741f8f2052792299f0af4d0754c7
Parth Vasdewani <imparthvasdewani@gmail.com>
2025-12-02 12:23:32 +0530

    Updated Django settings for celery

--- tick_system/settings.py
+++ tick_system/settings.py
@@ -128,3 +128,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Celery Configuration Options
+CELERY_TIMEZONE = "Asia/Kolkata"
+
+# Celery / Redis settings
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "django-db"
+
+
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Kolkata'
