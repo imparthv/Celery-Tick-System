85b4522b6eec9c239706b6ce66bea8d4af8f4102
Parth Vasdewani <imparthvasdewani@gmail.com>
2025-12-03 10:10:33 +0530

    Added MySQL db and connected it with django

--- tick_system/settings.py
+++ tick_system/settings.py
@@ -83,12 +83,25 @@ WSGI_APPLICATION = 'tick_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Connecting data with mysql db
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tick_engine',
+        'USER': 'root',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '3300',
     }
 }
+
 
 
 # Password validation
